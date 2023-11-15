c18045e0c6179f5e877a931e9283584f7b9f92bb
a1fsNightlife <!H22akdsjg>
2023-11-15 16:34:05 +0100

    impl

--- src/app/overview-page/overview-page.component.ts
+++ src/app/overview-page/overview-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderNavComponent } from '../header-nav/header-nav.component';
 
 @Component({
   selector: 'app-overview-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderNavComponent],
   templateUrl: './overview-page.component.html',
-  styleUrl: './overview-page.component.scss'
+  styleUrl: './overview-page.component.scss',
 })
-export class OverviewPageComponent {
-
-}
+export class OverviewPageComponent {}

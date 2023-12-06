a8a0002e98034cb725341c20068eb76447965355
a1fsNightlife <!H22akdsjg>
2023-12-06 09:55:37 +0100

    curr state

--- src/app/overview-page/overview-page.component.ts
+++ src/app/overview-page/overview-page.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderNavComponent } from '../header-nav/header-nav.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-overview-page',
   standalone: true,
-  imports: [CommonModule, HeaderNavComponent],
+  imports: [CommonModule, HeaderNavComponent, MatButtonModule, MatCardModule],
   templateUrl: './overview-page.component.html',
   styleUrl: './overview-page.component.scss',
 })
-export class OverviewPageComponent {}
+export class OverviewPageComponent {
+  public dogs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
